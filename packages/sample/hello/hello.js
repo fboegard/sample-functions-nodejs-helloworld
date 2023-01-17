2aad8c1cd47986f77a7dee399500be5bfc6e36e9
Fredrik Boegard <fredrik@boegard.com>
2023-01-17 11:14:20 +0100

    Update hello.js

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,6 +1,6 @@
 function main(args) {
     let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
+    let greeting = 'Hejsan svejsan ' + name + '!'
     console.log(greeting)
     return {"body": greeting}
   }
